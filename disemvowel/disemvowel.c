1b66df41e8928d26c9f648c8a695c1492fa85f0c
stev0531 <stev0531@morris.umn.edu>
2018-09-20 11:32:58 -0500

    Create 'disemvowel.c'
    We put logic into 'disemvowel.c' to get
    rid of vowels, and it compiles with no errors.
    Some tests are failing.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,45 @@
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #include "disemvowel.h"
 
+bool* isVowel(char *str){
+  if(str[0] == 'a' || str[0] == 'e' || str[0] == 'i' || str[0] == 'o' || str[0] == 'u'
+		  || str[0] == 'A' || str[0] == 'E'|| str[0] == 'I'|| str[0] == 'O'|| str[0] == 'U'){
+    return (bool*)true;
+  } else {
+    return (bool*)false;
+  }
+}
+
 char *disemvowel(char *str) {
-  return (char*) "";
+  int size;
+  int vowels, i;
+  char *result;
+
+  size = strlen(str);
+
+  vowels = 0;
+  for(i=0; i<size; i++) {
+    if(str[0] == 'a' || str[0] == 'e' || str[0] == 'i' || str[0] == 'o' || str[0] == 'u'
+                  || str[0] == 'A' || str[0] == 'E'|| str[0] == 'I'|| str[0] == 'O'|| str[0] == 'U'){
+      ++vowels;
+    }
+  }
+
+  if(vowels >= size){
+    return (char*) "";
+  }
+
+  result = (char*) calloc(size-vowels+1, sizeof(char));
+
+  for(i=0; i<size; i++) {
+    if(str[0] == 'a' || str[0] == 'e' || str[0] == 'i' || str[0] == 'o' || str[0] == 'u'
+                  || str[0] == 'A' || str[0] == 'E'|| str[0] == 'I'|| str[0] == 'O'|| str[0] == 'U'){
+      result[i]=str[i+vowels];
+    }
+  }
+
+  return result;
 }
